a1487f2cc1d8de0207872a5d845708c3b62122ae
Jaeyeop <birdman9391@postech.ac.kr>
2017-10-12 13:04:53 -0700

    Preparation for syscall

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -5,6 +5,19 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+void syscall_halt();
+void syscall_exit();
+void syscall_exec();
+void syscall_wait();
+void syscall_create();
+void syscall_remove();
+void syscall_open();
+void syscall_filesize();
+void syscall_read();
+void syscall_write();
+void syscall_seek();
+void syscall_tell();
+void syscall_close();
 
 void
 syscall_init (void) 
@@ -16,5 +29,89 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  switch(*((int*)f->esp))
+  {
+  case SYS_HALT:
+    syscall_halt();
+  case SYS_EXIT:
+    syscall_exit();
+  case SYS_EXEC:
+    syscall_exec();
+  case SYS_WAIT:
+    syscall_wait();
+  case SYS_CREATE:
+    syscall_create();
+  case SYS_REMOVE:
+    syscall_remove();
+  case SYS_OPEN:
+    syscall_open();
+  case SYS_FILESIZE:
+    syscall_filesize();
+  case SYS_READ:
+    syscall_read();
+  case SYS_WRITE:
+    syscall_write();
+  case SYS_SEEK:
+    syscall_seek();
+  case SYS_TELL:
+    syscall_tell();
+  case SYS_CLOSE:
+    syscall_close();
+  dafault:
+    printf("Error loading syscall, syscall num : %d\n",*((int*)f->esp));
+  }
   thread_exit ();
+}
+
+void syscall_halt()
+{
+printf("halt\n");
+}
+void syscall_exit()
+{
+printf("exit\n");
+}
+void syscall_exec()
+{
+printf("exec\n");
+}
+void syscall_wait()
+{
+printf("wait\n");
+}
+void syscall_create()
+{
+printf("create\n");
+}
+void syscall_remove()
+{
+printf("remove\n");
+}
+void syscall_open()
+{
+printf("open\n");
+}
+void syscall_filesize()
+{
+printf("filesize\n");
+}
+void syscall_read()
+{
+printf("read\n");
+}
+void syscall_write()
+{
+printf("write\n");
+}
+void syscall_seek()
+{
+printf("seek\n");
+}
+void syscall_tell()
+{
+printf("tell\n");
+}
+void syscall_close()
+{
+printf("close\n");
 }
